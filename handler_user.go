b349fc3516d1ccb359546fc5bc0619b3cdbded47
mrzalais <lejnieksedijs@gmail.com>
2023-06-07 17:08:25 +0300

    Add api keys and get user endpoint

--- handler_user.go
+++ handler_user.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
+	"github.com/mrzalais/go-rss-aggregator/internal/auth"
 	"github.com/mrzalais/go-rss-aggregator/internal/database"
 	"net/http"
 	"time"
@@ -31,6 +32,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %s", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Authentication error: %s", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %s", err))
 		return
 	}
 
